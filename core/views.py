f7e0449cd2c75bf092beda45ae68632e51407980
DevJuanDuarte <juan.devcontact@gmail.com>
2022-10-12 23:54:37 -0500

    Creación web personal

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+    
