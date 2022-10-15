042c06977859f73fb5d12e3569023e72484838b6
DevJuanDuarte <juan.devcontact@gmail.com>
2022-10-14 23:59:03 -0500

    Panel de administrador, campos especiales

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from portfolio.models import Project
 
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
 # Register your models here.
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
