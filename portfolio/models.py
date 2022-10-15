2920594735a9d55335770de978cea4445cb67d33
DevJuanDuarte <juan.devcontact@gmail.com>
2022-10-15 00:53:07 -0500

    Cargar datos de db, archivos media y enlaces de proyectos

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,13 +6,15 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name = 'Titulo')
     description = models.TextField(verbose_name = 'Descripcion')
-    image = models.ImageField(verbose_name = 'Imagen')
+    image = models.ImageField(verbose_name = 'Imagen', upload_to = 'Project')
+    link = models.URLField(null=True, blank=True, verbose_name = 'Direccion web')
     created = models.DateTimeField(auto_now_add=True, verbose_name = 'Fecha de creación')
     updated = models.DateTimeField(auto_now=True, verbose_name = 'Fecha de modificación')
 
     class Meta:
         verbose_name = 'Proyecto'
         verbose_name_plural = 'Proyectos'
+        ordering = ['-created']
 
 
     def __str__(self) -> str:
